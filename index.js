2f53f3077d1f58dc12eea71f63f0f8bad345f609
Hardik singh <singhhardik14@gmail.com>
2021-07-27 13:02:05 +0530

    Fixed kukai wallet not responding problem

--- index.js
+++ index.js
@@ -1,30 +1,31 @@
-const bakerAddress = "tz1MJx9vhaNRSimcuXPK2rW4fLccQnDAnVKJ"; 
-const bakerName = "Delegation Example DApp"; 
+const options = {
+  name: "MyAwesomeDapp",
+  preferredNetwork: beacon.NetworkType.MAINNET,
+};
+let myAddress;
 
-    // Initiate DAppClient
-    const client = new beacon.DAppClient({
-      name: bakerName,
-    });
+const dAppClient = new beacon.DAppClient(options);
 
-    const delegate = () => {
-      client.requestOperation({
-        operationDetails: [
-          {
-            kind: beacon.TezosOperationType.DELEGATION,
-            delegate: bakerAddress,
-          },
-        ],
-      });
-    };
-
-    document.getElementById("connect").addEventListener("click", () => {
-      client.getActiveAccount().then((activeAccount) => {
-        if (activeAccount) {
-          delegate();
-        } else {
-          client.requestPermissions().then((permissions) => {
-            delegate();
-          });
-        }
-      });
-    });
+async function loadEdit() {
+  const activeAccount = await dAppClient.getActiveAccount();
+  if (activeAccount) {
+    console.log(activeAccount)
+    console.log("Already connected:", activeAccount.address);
+    myAddress = activeAccount.address;
+    document.getElementById("connect").disabled = true; 
+    document.getElementById("connect").style.opacity = "0.5";
+    document.getElementById("connect").innerHTML= `<img src="./assest/beacon.png"> Already Connected`; 
+  } 
+  else {
+      const permissions = await dAppClient.requestPermissions();
+      console.log("New connection:", permissions.address);
+      myAddress = permissions.address;
+  }
+}
+loadEdit();
+function load() {
+  dAppClient.clearActiveAccount();
+  document.getElementById("connect").disabled = false; 
+    document.getElementById("connect").style.opacity = "1";
+    document.getElementById("connect").innerHTML= `<img src="./assest/beacon.png"> Connect With Tezos Wallet`; 
+}
